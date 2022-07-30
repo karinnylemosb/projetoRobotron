3613491edc64612b1b74f5c4e9929c486e4e0104
Karinny Lemos <kakalemos.ti@gmail.com>
2022-07-30 10:50:16 -0300

    Melhorando a função de estasticas

--- js/main.js
+++ js/main.js
@@ -32,16 +32,16 @@ const pecas = {
   },
 };
 
-const controle = document.querySelectorAll('.controle-ajuste');
+const controle = document.querySelectorAll(".controle-ajuste");
 //Pegando toda a classe de controle-ajuste e depois a data-controle
-const estatisticaElement = document.querySelectorAll('[data-estatistica]');
+const estatisticaElement = document.querySelectorAll("[data-estatistica]");
 const producao = document.querySelector("#producao");
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
     manipulaDados(evento.target.textContent, evento.target.parentNode);
     //Target (no console) mostra exatamente qual botão foi apertado. O data-set.controle busca o data-controle dentro do html. o evento seguinte, o parentNode, buscou o pai do elemento que foi clicado, o outro parâmetro da função, que criou o parâmetro 'controle'
-    atualizaEstatistica(evento.target.dataset.peca);
+    atualizaEstatistica(evento.target.dataset.peca, evento.target.textContent);
   });
 });
 
@@ -58,21 +58,41 @@ function manipulaDados(operacao, controle) {
     if (pecaContador.value > 0) {
       pecaContador.value = parseInt(pecaContador.value) - 1;
     } else {
-      pecaContador.value = "00"
+      pecaContador.value = "00";
     }
   } else if (operacao === "+") {
     pecaContador.value = parseInt(pecaContador.value) + 1;
   }
 }
 
-function trocaImagem(cor){
-    document.querySelector(".robo").src="img/Robotron 2000 - " + cor + ".png";
- }
+function atualizaEstatistica(pecaContador, operacao) {
+  switch (operacao) {
+    case "-":
+      subtrairEstatisticas(pecaContador);
+      break;
+    case "+":
+      incrementarEstatisticas(pecaContador);
+      break;
+  }
+}
 
-function atualizaEstatistica(pecaContador) {
+function incrementarEstatisticas(pecaContador) {
+  estatisticaElement.forEach((elemento) => {
+    elemento.textContent =
+      parseInt(elemento.textContent) +
+      pecas[pecaContador][elemento.dataset.estatistica];
+  });
+}
 
-  estatisticaElement.forEach( (elemento) => {
-      elemento.textContent = parseInt(elemento.textContent) + pecas[pecaContador][elemento.dataset.estatistica];
-  })
+function subtrairEstatisticas(pecaContador) {
+  estatisticaElement.forEach((elemento) => {
+    elemento.textContent =
+      parseInt(elemento.textContent) -
+      pecas[pecaContador][elemento.dataset.estatistica];
+  });
+}
 
+
+function trocaImagem(cor) {
+  document.querySelector(".robo").src = "img/Robotron 2000 - " + cor + ".png";
 }
